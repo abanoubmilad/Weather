544e7d2a9d1d63827d26eeb61c00d764480d065b
bono <abanoubcs@gmail.com>
2018-01-23 21:21:55 +0200

    Add forecast adapter

---
+++ app/src/main/java/werpx/weather/ForecastAdapter.java
@@ -0,0 +1,53 @@
+package werpx.weather;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class ForecastAdapter extends Adapter<Forecast> {
+
+    private final SimpleDateFormat dateFormater = new SimpleDateFormat("yyyy.MM.dd HH.mm.ss");
+
+    public ForecastAdapter(Context context, ArrayList<Forecast> forecastDays) {
+        super(context, 0, forecastDays);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        Forecast forecastDay = getItem(position);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item_city, parent, false);
+            holder = new ViewHolder();
+            holder.timestamp = (TextView) convertView.findViewById(R.id.timestamp);
+            holder.temp = (TextView) convertView.findViewById(R.id.day_temp);
+            holder.min = (TextView) convertView.findViewById(R.id.min_temp);
+            holder.max = (TextView) convertView.findViewById(R.id.max_temp);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        holder.timestamp.setText(dateFormater.format(new Date(forecastDay.getTimeStamp())));
+        holder.temp.setText(forecastDay.getDayTemp()+"");
+        holder.min.setText(forecastDay.getMinTemp()+"");
+        holder.max.setText(forecastDay.getMaxTemp()+"");
+
+        return convertView;
+    }
+
+    private static class ViewHolder {
+        TextView timestamp;
+        TextView temp;
+        TextView min;
+        TextView max;
+
+    }
+}
